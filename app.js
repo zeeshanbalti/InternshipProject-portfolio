9704a5e7ae8d44c56f27e7c7e7468bd771172cb1
Zeeshan Haider <122179093+zeeshanbalti@users.noreply.github.com>
2023-10-16 05:56:27 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+
+window.addEventListener("scroll",  scroll)
+
+let scrowl = () => {
+    var scroll = document.getElementById('scroll')
+}
+
+
+// Skill Section 
+
+// For HTML
+
+ const skillRatingHTML0 = 85;
+
+ const fillElement0 = document.querySelector('.fill0');
+const percentageElement0 = document.querySelector('.percentage0');
+
+fillElement0.style.width = `${skillRatingHTML0}%`;
+percentageElement0.innerText = `${skillRatingHTML0}%`;
+
+// For CSS
+
+const skillRatingHTML1 = 85;
+
+const fillElement1 = document.querySelector('.fill1');
+const percentageElement1 = document.querySelector('.percentage1');
+
+fillElement1.style.width = `${skillRatingHTML1}%`;
+percentageElement1.innerText = `${skillRatingHTML1}%`;
+
+// For JS
+
+const skillRatingHTML2 = 75;
+
+const fillElement2 = document.querySelector('.fill2');
+const percentageElement2 = document.querySelector('.percentage2');
+
+fillElement2.style.width = `${skillRatingHTML2}%`;
+percentageElement2.innerText = `${skillRatingHTML2}%`;
+
+//  For React
+
+const skillRatingHTML3 = 85;
+
+const fillElement3 = document.querySelector('.fill3');
+const percentageElement3 = document.querySelector('.percentage3');
+
+fillElement3.style.width = `${skillRatingHTML3}%`;
+percentageElement3.innerText = `${skillRatingHTML3}%`;
+
